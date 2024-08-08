4792c9fad027c9992a35f33c8fd334f1ee4fad04
Michel <michel@Michels-MBP.localdomain>
2024-08-08 13:15:58 -0400

    Basic Structure created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "./src/styles/global.css";`
+        }
+      }
+    }
+  }
+});
